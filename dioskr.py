246115ee66520c44d5c4ca146c774e4483a8cabd
Spyridon Papageorgiou <spapageo@prometheus.lan>
2014-11-19 02:20:05 +0200

    -Default values for block size and input file are now given to ArgumentParser -Added 'type' field in ArgumentParser to validate input variables at load time -Removed file size limitation of 10MB, now allowing 4K block size at the very least -Substituted double quotes on strings with single quotes -Added some initial exceptions and handling - far from over, but ok for initial debugging -Added handle for BlockStore class - implemented in layer_block.py

--- dioskr.py
+++ dioskr.py
@@ -1,78 +1,95 @@
 #!/usr/bin/python
 
-#TODO: create a torrent file for reconstruction purposes!!!
+#TODO: create a torrent file for reconstruction purposes!
 
 import sys
 import argparse
 import os
 import hashlib
+import inspect
+from layer_block import BlockStore
+
 
 output_dir = "/mnt/shared_volume/dskrs_field"
 
+def input_check(arg):
+    #check if file exists
+    if os.path.isfile(arg) is False:
+        raise argparse.ArgumentTypeError('Input file does not exist')
+
+    return arg
+
+
+def block_check(arg):
+    value = int(arg)
+    if value <= 0:
+        raise argparse.ArgumentTypeError('Block size should be a positive value')
+
+    if ((value % 4096) != 0):
+        raise argparse.ArgumentTypeError('Block size should be 4K aligned')
+
+    return value
+
 def init_vars():
     parser = argparse.ArgumentParser(description='dioskr input parameters.')
-    parser.add_argument('input_file', help='input file path')
-#    parser.add_argument('output_dir', help='output directory')
-    parser.add_argument('block_size', help='size of block deduplication in bytes')
+    parser.add_argument('input_file', default='', type=input_check, help='input file path')
+    parser.add_argument('block_size', default='4096', type=block_check, help='size of block deduplication in bytes')
+    #parser.add_argument('output_dir', help='output directory')
     args = parser.parse_args()
 
     return args.input_file, args.block_size
 
 def main():
-    input_file = ''
-    block_sz = 4096
+    print '=======dioskrS v0.1========'
+    input_file, block_size = init_vars()
 
-    print "=======dioskrS v0.1========"
+    file_size = os.stat(input_file).st_size
+    print 'file_size: %d' % file_size
 
-    input_file, block_sz = init_vars()
-
-    file_sz = os.stat(input_file).st_size
-    print "file_sz: %d" % file_sz
-    if (file_sz < 10485760):
-        print "Please provide a file larger than 10MB!"
-        sys.exit(0)
-
-    block_sz = int(block_sz)
-    #Check if block size is greater than 512 bytes
-    if (block_sz < 4096):
-        print "Block size should be > 4096 bytes"
-        sys.exit(0)
-
-    #Check if block size is aligned
-    if ((block_sz % 4096) != 0):
-        print "Block size provided is not 4KB aligned"
+    #Should handle this later on.
+    if (file_size < block_size):
+        print 'File provided is smaller than the deduplication block size.'
         sys.exit(0)
 
     #file_fp = open(input_file, "rb") #alternate open, built-in, not direct!
-    file_fp = os.open(input_file, os.O_DIRECT | os.O_RDONLY)
-    
+    try: 
+        file_fp = os.open(input_file, os.O_DIRECT | os.O_RDONLY)
+    except Exception as e:
+        frame = inspect.currentframe()
+        info = inspect.getframeinfo(frame)
+        print '\t[fopen: an %s exception occured | line: %d]' % (type(e).__name__, info.lineno)
+        sys.exit(0)
+
     if not (os.path.isdir(output_dir)):
-        print "Output directory \"%s\" does not exist. Will create.." % output_dir
+        print 'Output directory "%s" does not exist. Will create..' % output_dir
         os.makedirs(output_dir)
+
 
     blocks_nr = 0
     while True:
         #block = file_fp.read(block_size)
-        block = os.read(file_fp, block_sz)
-        blocks_nr += 1
+        try: 
+            block = os.read(file_fp, block_size)
+            blocks_nr += 1
+            hash_object = hashlib.sha512(block)
+            hex_dig = hash_object.hexdigest()
+        except Exception as e:
+            frame = inspect.currentframe()
+            info = inspect.getframeinfo(frame)
+            print '\t[read: an %s exception occured | line: %d]' % (type(e).__name__, info.lineno)
+            break
 
-        hash_object = hashlib.sha512(block)
-        hex_dig = hash_object.hexdigest()
         #print"%d: %s" % (blocks_nr, hex_dig)
-
-        # Check first 8 characters in output_dir, move one level down
-        # Check next 8 characters in output_dir/something, move one level down
-        # Check if file exists, if not, create
-
 
 
         if block == "":
             break
 
-    print "Finished reading file! Number of %d blocks: %d" % (block_sz, blocks_nr)
-
+    print 'Finished reading file! Number of %d blocks: %d' % (block_size, blocks_nr)
 
 
 
 if __name__ == "__main__":
 	main()
+
+
---
+++ layer_block.py
@@ -0,0 +1,4 @@
+class BlockStore:
+    def __init__(self, a1, a2):
+        self.a1 = 2
+        self.a2 = 3
